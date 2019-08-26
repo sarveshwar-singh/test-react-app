208e59451ad76948eea9fa95f14c3618a77c4fec
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-08-26 16:46:15 +0900

    Updates

---
+++ precache-manifest.6cacefd2822b34920e499b1f84c5c094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c727110359a0b3b731dcd9078229f918",
+    "url": "/test-react-app/index.html"
+  },
+  {
+    "revision": "7431fdb2f76397740c1d",
+    "url": "/test-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d66ce25185298c0f0943",
+    "url": "/test-react-app/static/js/2.afeef429.chunk.js"
+  },
+  {
+    "revision": "7431fdb2f76397740c1d",
+    "url": "/test-react-app/static/js/main.f963a080.chunk.js"
+  },
+  {
+    "revision": "f26c25147ca4cb384b89",
+    "url": "/test-react-app/static/js/runtime~main.785c0757.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
